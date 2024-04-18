889e072625cd2a1316ce13a47d7c9efb40120f93
Tarique <tarique123@example.com>
2024-04-18 11:35:20 +0530

    temporarily change rupee to dollar in Card

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -52,7 +52,7 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
 
             {!hidePrice && <div className="flex gap-2">
                 <p className='p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60 line-clamp-1'>
-                    {event.isFree ? "FREE" : `₹${event.price}`}
+                    {event.isFree ? "FREE" : `$${event.price}`}
                 </p>
                 <p className='p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1'>
                     {event.category.name}
